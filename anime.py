afb546f6de98bd8b945e02a4bd52ba97d5f2bcde
Om Kundgolkar <teroequinox@gmail.com>
2023-03-21 21:02:09 +0530

    initial commit

--- anime.py
+++ anime.py
@@ -1,1 +1,26 @@
+import random as cum
+anime = ['ONE PIECE','naruto','Chainsaw Man','Demon Slayer','Grand Blue']
+
+def randanimesuggestio():
+    def randanime():
+        print(f'You should try watching {cum.choice(anime)}');
+        print('  ')
+
+    def check():
+        w = input('Already watched it?\nY/N');
+
+        if w != 'Y' and w != 'y':
+            if w != 'N' and w != 'n':
+                print('INVALID INPUT');
+            else:
+                print('Cool go ahead and watch it then!!')
+
+
+        else:
+            print('Ohh then,')
+            randanime();
+    randanime();
+    check();
+
+randanimesuggestio()
 
